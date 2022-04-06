88293255b8c4cb2cf962904b654fb543019eecad
bumbleIV <linganda@msu.edu>
2022-04-06 00:27:12 -0400

    reconfigure directories

--- socketio/main.py
+++ socketio/main.py
@@ -1,32 +1,29 @@
-from flask import Flask
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+# https://flask-socketio.readthedocs.io/en/latest/
+# https://github.com/socketio/socket.io-client
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
 
-
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-    send(message, broadcast=True)
-
-# index route
+app.config['SECRET_KEY'] = 'jsbcfsbfjefebw237u3gdbdc'
+socketio = SocketIO(app)
 
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def hello():
+    return render_template('./index.html')
+
+
+def messageRecived():
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('recived my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageRecived)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
-
-
-
-# button iteraction:
-# use an html form to send a message to the server <form>
-# look up hmtl form pattern
-# wrap button in a form
-
-# use javascript as an alternative to html form
+    socketio.run(app, debug=True)
