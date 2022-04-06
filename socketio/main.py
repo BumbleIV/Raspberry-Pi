57d57c2f1947590a86b989b48dac9c1d22cbac84
bumbleIV <linganda@msu.edu>
2022-04-06 00:54:58 -0400

    update

--- socketio/main.py
+++ socketio/main.py
@@ -8,28 +8,19 @@ app.config['SECRET_KEY'] = 'jsbcfsbfjefebw237u3gdbdc'
 socketio = SocketIO(app)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    if request.method == 'POST':
-        date = request.form['date']
-        title = request.form['blog_title']
-        post = request.form['blog_main']
-        post_entry = models.BlogPost(date=date, title=title, post=post)
-        db.session.add(post_entry)
-        db.session.commit()
-        return redirect(url_for('database'))
-    else:
-        return render_template('./index.html')
+    return render_template('./index.html')
 
 
 def messageRecived():
     print('message was received!!!')
 
 
-@socketio.on('my event')
+@socketio.on('My Event')
 def handle_my_custom_event(json):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageRecived)
+    print('Received' + str(json))
+    socketio.emit('My Response', json, callback=messageRecived)
 
 
 if __name__ == '__main__':
