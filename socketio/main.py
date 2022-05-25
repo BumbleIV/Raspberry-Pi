0d5e9450ef761f4987bd4a3bdfe7f2ead738fc1e
bumbleIV <linganda@msu.edu>
2022-05-25 02:11:06 -0500

    update

--- socketio/main.py
+++ socketio/main.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template, request, Response, redirect, url_for
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'jsbcfsbfjefebw237u3gdbdc'
+app.config['SECRET_KEY'] = 'so secret'
 socketio = SocketIO(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+def index():
     return render_template('./index.html')
 
 
-def messageRecived():
-    print('message was received!!!')
+def message_received():
+    print('Message Received')
 
 
 @socketio.on('My Event')
 def handle_my_custom_event(json):
     print('Received' + str(json))
-    socketio.emit('My Response', json, callback=messageRecived)
+    socketio.emit('My Response', json, callback=message_received)
 
 
 if __name__ == '__main__':
